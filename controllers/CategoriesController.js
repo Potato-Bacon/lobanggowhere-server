c69a63cdb656ec9f652656fbda78c9000834cb56
junjiecheo12 <wkcheojj@gmail.com>
2022-09-16 14:33:37 +0800

    complete categories controller

--- controllers/CategoriesController.js
+++ controllers/CategoriesController.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const Category = require("../models/CategorySchema");
 const router = express.Router();
 
 //* Search for deals by search category
 //? No auth req
-router.get("/", (req, res) => {
-  res.send({ deals: "individual" });
+router.get("/", async (req, res) => {
+  try {
+    const categories = await Category.find();
+    res.status(200).send(categories);
+  } catch (error) {
+    res.status(500).send({ message: error });
+  }
 });
 
 module.exports = router;
