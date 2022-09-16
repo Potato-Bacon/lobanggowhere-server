7b114ff3199bcb52f26fb79112227c9999cd33d2
junjiecheo12 <wkcheojj@gmail.com>
2022-09-16 23:14:29 +0800

    edit user schema

--- models/UserSchema.js
+++ models/UserSchema.js
@@ -4,10 +4,9 @@ const userSchema = new mongoose.Schema(
   {
     userName: { type: String, required: true },
     password: { type: String, required: true },
-    submissionEvent: [String],
-    submissionDeal: [String],
+    submissions: [String],
     likes: [String],
-    emailAddress: { type: String, required: true },
+    email: { type: String, required: true },
     dateOfBirth: { type: String, required: true },
     avatar: { type: Number, default: 0 },
     infringement: { type: Number, default: 0 },
