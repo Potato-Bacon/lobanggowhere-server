9be7d5411ddbe49307847dbb2165c8a4b23c2d02
potatobacon <andylohbiz@gmail.com>
2022-09-16 16:29:04 +0800

    submission route updated for POST

--- controllers/SubmissionsController.js
+++ controllers/SubmissionsController.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const Deals = require("../models/DealsSchema");
 const router = express.Router();
 
 //* Add a deal
 router.post("/", (req, res) => {
-  res.send({ submit: "submission" });
+  const newSubmission = req.body;
+  console.log("newSubmission", newSubmission);
+  Deals.create(newSubmission, (error, submission) => {
+    if (error) {
+      res.status(500).send({ error });
+    } else {
+      res.status(201).send(submission);
+    }
+  });
 });
 module.exports = router;
