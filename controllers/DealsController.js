67de7d9ea0b3c3741b748e7d711e9f7b798fe920
junjiecheo12 <wkcheojj@gmail.com>
2022-09-17 23:20:00 +0800

    complete get deal by id

--- controllers/DealsController.js
+++ controllers/DealsController.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const Deals = require("../models/DealsSchema");
 const router = express.Router();
 
 //* Search for deals by id
 //? No auth req
-router.get("/:id", (req, res) => {
-  res.send({ deals: "individual" });
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const deal = await Deals.findById(id).populate("category");
+  console.log(typeof id);
+  res.send(deal);
 });
 
 //* User edit post by id (verify user is owner)
