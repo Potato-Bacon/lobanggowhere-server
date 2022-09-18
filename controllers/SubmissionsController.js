73a60fa254a599420bba73ab0be5ff6bf1d80bff
potatobacon <andylohbiz@gmail.com>
2022-09-19 02:11:48 +0800

    installed multer and uuid 1

--- controllers/SubmissionsController.js
+++ controllers/SubmissionsController.js
@@ -9,25 +9,25 @@ const upload = multer({ dest: "uploads/" });
 router.post("/", upload.single("img"), (req, res) => {
   const newSubmission = req.body;
   const newImage = req.file;
-  console.log("newImage", newImage, "newSubmission", newSubmission);
+  // console.log("newImage", newImage, "newSubmission", newSubmission);
 
-  multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, "uploads");
-    },
-    filename: (req, file, cb) => {
-      const { originalname } = file;
-      cb(null, `${uuid()} - ${originalname} `);
-    },
-  });
-
-  // console.log("newSubmission", newSubmission);
-  // Deals.create(newSubmission, (error, submission) => {
-  //   if (error) {
-  //     res.status(500).send({ error });
-  //   } else {
-  //     res.status(201).send(submission);
-  //   }
+  // multer.diskStorage({
+  //   destination: (req, file, cb) => {
+  //     cb(null, "uploads");
+  //   },
+  //   filename: (req, file, cb) => {
+  //     const { originalname } = file;
+  //     cb(null, `${uuid()} - ${originalname} `);
+  //   },
   // });
+
+  console.log("newSubmission", newSubmission);
+  Deals.create(newSubmission, (error, submission) => {
+    if (error) {
+      res.status(500).send({ error });
+    } else {
+      res.status(201).send(submission);
+    }
+  });
 });
 module.exports = router;
