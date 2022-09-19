4bb73fef412eaf82e05aa76051d21f485b5293a1
potatobacon <andylohbiz@gmail.com>
2022-09-20 02:13:28 +0800

    account details done, change password successful

--- controllers/AccountsController.js
+++ controllers/AccountsController.js
@@ -1,15 +1,33 @@
 const express = require("express");
+const User = require("../models/UserSchema");
+const bcrypt = require("bcrypt");
+
 const router = express.Router();
 
 //* Show personal account details
 //? No auth req (might not need an express route then?)
-router.get("/", (req, res) => {
-  res.send({ users: "individual" });
-});
 
 //* Change account password (or other details maybe?) by id
-router.put("/:id", (req, res) => {
-  res.send({ users: "individual" });
+router.post("/verify", async (req, res) => {
+  const { userName, password } = req.body;
+  const foundUser = await User.findOne({ userName: userName }).exec();
+  const match = await bcrypt.compare(password, foundUser.password);
+  if (match) {
+    res.status(200).json({ msg: "verified" });
+  } else {
+    res.status(401).json({ msg: "wrong password" });
+  }
+});
+
+router.put("/changepassword", async (req, res) => {
+  const { userName, newPassword } = req.body;
+  const updatePassword = await User.findOneAndUpdate(
+    {
+      userName: userName,
+    },
+    { password: bcrypt.hashSync(newPassword, 10) }
+  ).exec();
+  res.status(200).json(updatePassword);
 });
 
 //* Delete account by id
