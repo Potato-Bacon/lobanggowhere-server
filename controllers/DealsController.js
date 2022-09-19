9c94340cfb55e65a74526a0fbe3e494cc357ed58
junjiecheo12 <wkcheojj@gmail.com>
2022-09-19 17:15:31 +0800

    add remove route for deals detail

--- controllers/DealsController.js
+++ controllers/DealsController.js
@@ -13,19 +13,36 @@ router.get("/:id", async (req, res) => {
 
 //* User edit post by id (verify user is owner)
 //watchList
-router.put("/:id", async (req, res) => {
+router.put("/addtowatchlist/:id", async (req, res) => {
   const { id } = req.params;
   const body = req.body;
   try {
-    const updateWatchList = await User.findByIdAndUpdate(id, body, {
-      new: true,
-    });
-    const check = () => {
-      if (body.watchList) {
-        return res.status(201).send(updateWatchList);
+    const addToWatchList = await User.findOneAndUpdate(
+      { userName: id },
+      { $addToSet: { watchList: body } },
+      {
+        new: true,
       }
-    };
-    check();
+    );
+    res.status(201).send(addToWatchList);
+  } catch (error) {
+    res.status(400).send({ error });
+  }
+});
+
+router.put("/removefromwatchlist/:id", async (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  console.log(body, "body");
+  console.log(typeof id, "id");
+
+  try {
+    const removeToWatchList = await User.findOneAndUpdate(
+      { userName: id },
+      { $pull: { watchList: { $in: body } } }
+    );
+    console.log("remove", removeToWatchList);
+    res.status(201).send(removeToWatchList);
   } catch (error) {
     res.status(400).send({ error });
   }
