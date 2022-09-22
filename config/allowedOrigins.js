9eeb19cdf913535aab8b3df7642ac07972590920
Ernest-Low <ernestlow32@gmail.com>
2022-09-22 18:38:43 +0800

    testing cors changes for vercel

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,8 @@ const allowedOrigins = [
   //* Add deployment website url when deploying
   "http://localhost:5173",
   "https://lobanggowhere-client.vercel.app",
+  "https://lobanggowhere-client.vercel.app/",
+  "https://lobanggowhere-client.vercel.app/login",
 ];
 
 module.exports = allowedOrigins;
