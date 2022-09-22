1db5955f32b5e535440fe3b1b88acf3932909a6a
Ernest-Low <ernestlow32@gmail.com>
2022-09-22 18:49:47 +0800

    testing another cors setting change

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
   //* Add deployment website url when deploying
   "http://localhost:5173",
   "https://lobanggowhere-client.vercel.app",
-  "https://lobanggowhere-client.vercel.app/",
+  "https://lobanggowhere-client.vercel.app/:path",
   "https://lobanggowhere-client.vercel.app/login",
 ];
 
