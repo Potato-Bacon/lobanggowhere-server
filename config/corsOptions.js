00ab33717ede198bc236208c1cafff66f0748a12
Ernest-Low <ernestlow32@gmail.com>
2022-09-22 22:47:23 +0800

    another test without wildcard not trying that again

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -5,14 +5,13 @@ const corsOptions = {
   headers: [
     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
   ],
-  origin: "*",
-  // origin: (origin, callback) => {
-  //   if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-  //     callback(null, true);
-  //   } else {
-  //     callback(new Error("Not allowed by CORS"));
-  //   }
-  // },
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   optionsSuccessStatus: 200,
 };
 
