c7def3bd3d9eef4a8f625f4d0ea83aab6fef4142
Ernest-Low <ernestlow32@gmail.com>
2022-09-22 09:53:43 +0800

    added whitelist for vercel

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
   //* Add deployment website url when deploying
   "http://localhost:5173",
+  "https://lobanggowhere-client.vercel.app",
 ];
 
 module.exports = allowedOrigins;
